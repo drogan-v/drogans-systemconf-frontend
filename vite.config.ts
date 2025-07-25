20d62b26761780b0bf3d7a64ac20bba17a6e1dfc
drogan <artnov1kov@yandex.ru>
2025-07-25 14:26:37 +0300

    updated: allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,15 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['individual-projects.ru', 'www.individual-projects.ru'],
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
